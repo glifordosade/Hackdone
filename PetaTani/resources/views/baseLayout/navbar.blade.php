7b4bf7efe0f58f31caea64ed2444768854a56863
azizahND <145313514+azizahND@users.noreply.github.com>
2024-12-14 16:01:37 +0700

    done

--- PetaTani/resources/views/baseLayout/navbar.blade.php
+++ PetaTani/resources/views/baseLayout/navbar.blade.php
@@ -2,23 +2,27 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="img/Petatani.png" alt="Logo" style="width:80px; height:80px">
+                <img src="img/Petatani.png" alt="Logo" style="width:70px; height:70px">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse text-center" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#part1">Beranda</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#part2">Layanan</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#part3">Tentang Kami</a>
-                    </li>
-                </ul>
+                <!-- Tambahkan div pembungkus untuk menempatkan menu di tengah -->
+                <div class="mx-auto">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#part1">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#part2">Layanan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#part3">Tentang Kami</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Button Registrasi dan Masuk tetap di kanan -->
                 <span class="navbar-text">
                     <button type="button" class="btn btn-success" style="font-size:15px; background-color:#f7f7f7; color:#055C46">Registrasi</button>
                     <button type="button" class="btn btn-success" style="font-size:15px; background-color:#055C46;">Masuk</button>
